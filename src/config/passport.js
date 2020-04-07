f4083c26fc832fa5321eb542a6a3d45ad3557e62
Fraser R <frasbr@gmail.com>
2020-04-07 22:22:56 +1000

    Added Jwt authentication strategy to be used by protected routes

---
+++ src/config/passport.js
@@ -0,0 +1,34 @@
+const { JwtStrategy, ExtractJwt } = require("passport-jwt");
+const User = require("../models/User");
+
+const options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = process.env.SECRET;
+
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(options, (payload, done) => {
+      User.findOne({
+        where: {
+          id: payload.id
+        }
+      })
+        .then((user) => {
+          if (user) {
+            // Authorisation is granted
+            const userProfile = {
+              id: user.id,
+              username: user.username,
+              email: user.email
+            };
+            return done(null, userProfile);
+          }
+          // If no user then deny access
+          return done(null, false);
+        })
+        .catch((err) => {
+          return done(err, false);
+        });
+    })
+  );
+};
