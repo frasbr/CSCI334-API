e809d3b9a9b41146fcaf86c2c04e0369b6d25a19
Fraser R <frasbr@gmail.com>
2020-04-23 17:49:27 +1000

    updated user model

--- src/models/User.js
+++ src/models/User.js
@@ -6,32 +6,41 @@ const User = db.define("User", {
   id: {
     type: DataTypes.UUID,
     defaultValue: Sequelize.UUIDV4,
-    primaryKey: true
+    primaryKey: true,
+    allowNull: false
   },
   username: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false
   },
   email: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false
   },
   firstName: {
     type: DataTypes.STRING,
-    field: "first_name"
+    field: "first_name",
+    allowNull: false
   },
   lastName: {
     type: DataTypes.STRING,
-    field: "last_name"
+    field: "last_name",
+    allowNull: false
   },
   hash: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false
   },
   createdAt: {
     type: DataTypes.DATE,
-    field: "created_at"
+    defaultValue: Sequelize.DATE,
+    field: "created_at",
+    allowNull: false
   },
   updatedAt: {
     type: DataTypes.DATE,
-    field: "updated_at"
+    field: "updated_at",
+    allowNull: true
   }
 });
 
