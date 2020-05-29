283dfb1959cae66f5983921b5680afac7b21b29a
Fraser R <frasbr@gmail.com>
2020-05-29 11:29:46 +1000

    hghg

--- src/services/auth.js
+++ src/services/auth.js
@@ -55,20 +55,20 @@ const registerUser = (userDetails) => {
     });
 };
 
-const login = (username, password) => {
+const login = async (username, password) => {
     // Convert the provided password into a hash which can be compared with the hashed password stored within the database
     const hash = bcrypt.hashSync(password);
 
     // Look up the user in the database
-    return User.findOne({
-        where: {
-            hash,
-            $or: {
-                username: username,
-                email: username
+
+    try {
+        const user = await User.findOne({
+            where: {
+                hash,
+                [Op.$or]: [{ username }, { email: username }]
             }
-        }
-    }).then((user) => {
+        });
+
         if (!user) {
             // No user matches the provided details so return 401 (unauthorised)
             return responseGenerator(401, {
@@ -76,28 +76,25 @@ const login = (username, password) => {
             });
         }
 
-        // If we find a match then we generate a jwt to return to the user
-        try {
-            const payload = {
-                id: user.id,
-                username: user.username,
-                isLoginToken: true
-            };
+        const payload = {
+            id: user.id,
+            username: user.username,
+            isLoginToken: true
+        };
 
-            const token = jwt.sign(payload, process.env.SECRET, {
-                expiresIn: 3600
-            });
+        const token = jwt.sign(payload, process.env.SECRET, {
+            expiresIn: 3600
+        });
 
-            return responseGenerator(200, {
-                token
-            });
-        } catch (err) {
-            console.error(err);
-            return responseGenerator(500, {
-                message: "Something went wrong on our end"
-            });
-        }
-    });
+        return responseGenerator(200, {
+            token
+        });
+    } catch (err) {
+        console.error(err);
+        return responseGenerator(500, {
+            message: "Something went wrong on our end"
+        });
+    }
 };
 
 module.exports = {
