b510668bc746e4c00db50f9e7f49f10d5a47e031
Fraser R <frasbr@gmail.com>
2020-05-29 11:12:14 +1000

    uhidsaijhdsa

--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -1,3 +1,9 @@
 const Router = require("./Router");
 const Route = require("./Route");
 const ticketService = require("../services/ticket");
+
+const router = new Router("/ticket");
+
+const createTicket = new Route("post", "/create", true, async (req, res) => {});
+
+router.registerRoute(createTicket);
