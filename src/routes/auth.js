72e40fc4b09c58fcf0a30cef9ba37823c6611c76
Fraser R <frasbr@gmail.com>
2020-06-02 15:49:18 +1000

    dsadsad

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -41,7 +41,7 @@ const login = new Route("post", "/login", false, async (req, res) => {
 
     // Validate input
     if (!username || !password) {
-        res.status(422).json({
+        return res.status(422).json({
             message: "Please enter a valid username and password"
         });
     }
